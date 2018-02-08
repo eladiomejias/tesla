c13293608cdf6aed7d28881ea35a35aaad0d2cdb
Eladio Mejias <emejias13@ujap.edu.ve>
2018-02-08 14:31:45 -0400

    Fixing errors in History Component

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit{
 
   constructor() { }
 
