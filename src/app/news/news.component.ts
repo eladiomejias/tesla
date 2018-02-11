2a2af74443aca2a6bce155ac4a9d85f45dbe8976
Eladio Mejias <emejias13@ujap.edu.ve>
2018-02-10 22:28:14 -0400

    New data, img, style and display

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -18,18 +18,21 @@ import { userData } from "./user.json";
     <div class="mdl-cell mdl-cell--4-col-desktop mdl-cell--7-phone" *ngFor="let user of USER">
       <mat-card class="example-card">
         <mat-card-header>
-          <div mat-card-avatar class="example-header-image"></div>
+          <div mat-card-avatar class="card-image" [ngStyle]="{'background-image': 'url(' + user.profile_img +')', 'background-size': 'cover'}"></div>
           <mat-card-title>{{ user.name }} {{ user.second_name }}</mat-card-title>
           <mat-card-subtitle>{{ user.title }}</mat-card-subtitle>
         </mat-card-header>
         <img mat-card-image [src]="user.card_img" alt="">
         <mat-card-content>
+          <h5 class="card-content-title">{{ user.card_title }}</h5>
           <p>
             The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
             A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
             bred for hunting.
+            {{ user.description }}
           </p>
         </mat-card-content>
+        <mat-divider></mat-divider>
         <mat-card-actions>
           <button mat-button>ME GUSTA</button>
           <button mat-button>COMPARTIR</button>
@@ -49,6 +52,7 @@ export class NewsComponent implements OnInit{
           "profile_img": "../../assets/img/user/jason.png",
           "card_img": "../../assets/img/user/cover1.jpg",
           "title": "Tesla Enviroment",
+          "card_title": "Tesla, Inc. The Future.",
           "description": "Lorem ipsum",
           "age" : "24",
           "gender" : "male"
@@ -57,7 +61,8 @@ export class NewsComponent implements OnInit{
           "name" : "Kyle",
           "second_name": "Jack",
           "profile_img": "../../assets/img/user/jack.png",
-          "card_img": "../../assets/img/user/cover1.jpg",
+          "card_img": "../../assets/img/user/cover2.jpg",
+          "card_title": "Velocity with Model S.",
           "title": "Tesla Production",
           "description": "Lorem ipsum",
           "age" : "24",
@@ -67,11 +72,45 @@ export class NewsComponent implements OnInit{
           "name" : "Nick",
           "second_name": "Phillips",
           "profile_img": "../../assets/img/user/nick.png",
-          "card_img": "../../assets/img/user/cover1.jpg",
-          "title": "Tesla Thinks",
+          "card_img": "../../assets/img/user/cover3.jpg",
+          "card_title": "Meet the Starman.",
+          "title": "Tesla Thinks.",
           "description": "Lorem ipsum",
           "age" : "23",
           "gender" : "male"
+      },
+      {
+          "name" : "Edd",
+          "second_name": "William",
+          "profile_img": "../../assets/img/user/nick.png",
+          "card_img": "../../assets/img/user/cover4.jpg",
+          "card_title": "Creating the charge.",
+          "title": "Electric Engineer",
+          "description": "Lorem ipsum",
+          "age" : "23",
+          "gender" : "female"
+      },
+      {
+          "name" : "Addy",
+          "second_name": "Osmani",
+          "profile_img": "../../assets/img/user/addy.png",
+          "card_img": "../../assets/img/user/cover5.jpg",
+          "card_title": "Where to buy?",
+          "title": "Internal Creator",
+          "description": "Lorem ipsum",
+          "age" : "23",
+          "gender" : "male"
+      },
+      {
+          "name" : "Eva",
+          "second_name": "Houston",
+          "profile_img": "../../assets/img/user/eva.png",
+          "card_img": "../../assets/img/user/cover6.jpg",
+          "card_title": "The process of creativity.",
+          "title": "Process Observer",
+          "description": "Lorem Ipsum.",
+          "age" : "23",
+          "gender" : "female"
       }
   ];
 
