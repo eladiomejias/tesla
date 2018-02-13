168c4aa8fe6e922761352ef81df6e584cf72df80
Eladio Mejias <emejias13@ujap.edu.ve>
2018-02-12 21:46:56 -0400

    Comments

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -14,26 +14,7 @@ import { LangService } from '../shared/lang.service';
 })
 export class HistoryComponent implements OnInit{
 
-  constructor(private translate: TranslateService, private appService: LangService) {
-
-  // Gettings an array of lang
-/*  translate.addLangs(appService.getArray());
-
-  // Default Language when is not locally
-  translate.setDefaultLang("es");
-
-  // Using language
-  translate.use(appService.getValue());
-
-  // Default
-  let browserLang = translate.getBrowserLang();*/
-
-  // For another purpose
-
-  /*
-  translate.use(browserLang.match(/en/) ? browserLang : 'es');
-  */
-
+  constructor() {
  }
 
   ngOnInit() {
